63b3f5428a04d689bd58992f217ee52a9483a657
kaichi <kaichi1013@g.ecc.u-tokyo.ac.jp>
2025-11-01 00:59:37 +0900

    ルーターを修正

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import CoachRoute from "./app/routes/app/coach";
+import App from "./App";
 import "./index.css";
 import { ToastProvider } from "./ui/ToastProvider";
 
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <ToastProvider>
         <BrowserRouter>
           <Routes>
+            <Route path="/" element={<App />} />
             <Route path="/app/coach" element={<CoachRoute />} />
           </Routes>
         </BrowserRouter>
